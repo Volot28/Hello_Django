705fa1898cef736479e16cd7d8836eede79832c2
volot <Volot28@yandex.ru>
2022-07-20 06:32:20 +0300

    start_file

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    return render(request,'about.html',{'Hi':'piple'})
+def home(request):
+    return HttpResponse('This is my home')
